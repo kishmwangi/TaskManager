31fd8001e564716002185f7b99c3e9b0a5e901d8
kishmwangi <kishmwangi31@gmail.com>
2024-08-14 21:58:11 +0300

    new

--- taskmanager/cli.py
+++ taskmanager/cli.py
@@ -1,7 +1,6 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import click
 from taskmanager.manager import TaskManager
 from taskmanager.database import init_db
